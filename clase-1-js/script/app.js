7bc3e79785bb6823504963aa1d10b19d718b45f3
alejandro.vexler <alejandro.vexler@diveria.com>
2023-09-11 21:30:55 -0300

    clase 11/09

---
+++ clase-1-js/script/app.js
@@ -0,0 +1,42 @@
+// Var: se utilizaba antes de ECM6, nos permite inicializar una variable sin asignarle incialmente un valor, son variables de ambito global
+var nombre = "Giselle";
+nombre = "alejandro";
+
+// Let: es la nueva forma de declarar variables, son de ambito local y tambien nos permite inicializarla con un valor opcional
+let lastName;
+lastName = "llanos";
+lastName = "vexler";
+
+//const: constantes de solo lectura y de ambito local 
+const dni = "12345678";
+
+
+// operadores
+
+//operador de asignacion "="
+
+const direccion = "general paz 576";
+
+//operadores aritmeticos
+
+let suma = 2 + 2;
+
+let resta = 10 - 5;
+
+let multiplicacion = 2 * 2;
+
+// operadores unitarios
+
+// let resultado = 10;
+// console.log(resultado);
+// resultado++;
+// console.log(resultado);
+// resultado--;
+// console.log(resultado);
+
+let ganador = false;
+
+let mayorDeEdad = 10 !== "10";
+
+console.log(mayorDeEdad)
+
